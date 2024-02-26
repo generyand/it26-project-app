02e0debf5e7e1252f7eaa0f4f766aabffe6748e0
generyand <generyanaceron@gmail.com>
2024-02-26 13:32:56 +0800

    added a button for next page

--- app/src/main/java/com/example/thisapp/MainActivity.java
+++ app/src/main/java/com/example/thisapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.thisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
