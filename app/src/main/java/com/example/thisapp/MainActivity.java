fe19ad334f34dc433b807607fbf095aa344e252e
generyand <generyanaceron@gmail.com>
2024-03-05 15:51:23 +0800

    nigana na ang bottom nav

--- app/src/main/java/com/example/thisapp/MainActivity.java
+++ app/src/main/java/com/example/thisapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.thisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +12,13 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import com.example.thisapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
     public CardView java, javascript, nextjs;
     private Button button;
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,42 +30,66 @@ public class MainActivity extends AppCompatActivity {
         );
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.nextButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity2();
+        // Bottom Nav
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        repFragment(new HomeFragment());
+
+        binding.bottomNavView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                repFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.profile) {
+                repFragment(new ProfileFragment());
+            } else if (item.getItemId() == R.id.settings) {
+                repFragment(new SettingsFragment());
             }
+
+            return true;
         });
 
-        java = (CardView) findViewById(R.id.java);
-        java.setOnClickListener(this::onClick);
-
-        javascript = (CardView) findViewById(R.id.javascript);
-        javascript.setOnClickListener(this::onClick);
-
-        nextjs = (CardView) findViewById(R.id.nextjs);
-        nextjs.setOnClickListener(this::onClick);
+//        button = (Button) findViewById(R.id.nextButton);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openActivity2();
+//            }
+//        });
+//
+//        java = (CardView) findViewById(R.id.java);
+//        java.setOnClickListener(this::onClick);
+//
+//        javascript = (CardView) findViewById(R.id.javascript);
+//        javascript.setOnClickListener(this::onClick);
+//
+//        nextjs = (CardView) findViewById(R.id.nextjs);
+//        nextjs.setOnClickListener(this::onClick);
 
     }
 
-    public void openActivity2() {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
+    private void repFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_out, fragment);
+        fragmentTransaction.commit();
     }
 
-    public void onClick(View view) {
-        Intent i;
-
-        if (view.getId() == R.id.java) {
-            i = new Intent(this, Java.class);
-            startActivity(i);
-        } else if (view.getId() == R.id.javascript) {
-            i = new Intent(this, Javascript.class);
-            startActivity(i);
-        } else if (view.getId() == R.id.nextjs) {
-            i = new Intent(this, NextJS.class);
-            startActivity(i);
-        }
-    }
+//    public void openActivity2() {
+//        Intent intent = new Intent(this, Activity2.class);
+//        startActivity(intent);
+//    }
+//
+//    public void onClick(View view) {
+//        Intent i;
+//
+//        if (view.getId() == R.id.java) {
+//            i = new Intent(this, Java.class);
+//            startActivity(i);
+//        } else if (view.getId() == R.id.javascript) {
+//            i = new Intent(this, Javascript.class);
+//            startActivity(i);
+//        } else if (view.getId() == R.id.nextjs) {
+//            i = new Intent(this, NextJS.class);
+//            startActivity(i);
+//        }
+//    }
 }
