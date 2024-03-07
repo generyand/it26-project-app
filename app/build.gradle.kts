8e66236f6371511f9d91aa0117bf7480794eac14
generyand <generyanaceron@gmail.com>
2024-03-07 23:19:39 +0800

    last added is navigation side sa menu and the icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.google.android.material:material:1.2.0-alpha02")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.2.0-rc03")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.2.0-rc03")
 }
